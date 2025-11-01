67984da38c1890273ecf3718af5e10c78dbeeee6
MaiTranBaoTran <tranb2205917@student.ctu.edu.vn>
2025-11-01 11:28:27 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-//app.js
+// app.js
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
 
-//Middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-//Routes
+// Routes
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to contact book application." });
+  res.json({ message: "Welcome to the backend server!" });
 });
-
-//Routes
+// Routes
 const contactRoutes = require("./app/routes/contact.route");
 app.use("/api/contacts", contactRoutes);
+
+const ApiError = require("./app/api-error");
+
+// Middleware handle 404
+app.use((req, res, next) => {
+  // Nếu không khớp route nào
+  return next(new ApiError(404, "Resource not found"));
+});
+
+// Middleware xử lý lỗi cuối cùng
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
